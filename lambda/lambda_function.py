6c6227ffdcda89803cc585dc3088475c71fec119
Caroline <carolineschoolgm@gmail.com>
2025-03-10 21:43:58 -0400

    updated file

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CICD Lambda in my vscode!')
+        'body': json.dumps('Hello from CICD Lambda in my vscode software!')
     }
